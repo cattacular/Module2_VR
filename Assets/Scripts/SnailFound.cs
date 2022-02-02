eae3b3d5bd32a3ab829d7f1a0a2a7825e8a319bc
Chase Prochnow <46829087+cprochnow28@users.noreply.github.com>
2022-02-02 00:11:53 -0600

    fixed snail script

--- Assets/Scripts/SnailFound.cs
+++ Assets/Scripts/SnailFound.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class slugFound : MonoBehaviour
+public class SnailFound : MonoBehaviour
 {
+    private Vector3 spawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawn = transform.position; 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        if(spawn != transform.position)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            enabled = false;
+        }
     }
 }
